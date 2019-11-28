ed5b9d16143b6e24250d0537b462c5d8357d348a
Steve <steve.azzolin@libero.it>
2019-11-28 10:23:45 +0100

    Add Connection script and gitignore for the file db_password

---
+++ src/models/connection.js
@@ -0,0 +1,30 @@
+//Create the connection Pool for the DB
+var assert = require('assert');
+const config = require('../config');
+const Logger = require('../loaders/logger');
+var { MongoClient } = require("mongodb");
+
+var mongodb = null;
+
+async function connect(){
+    password = config.db_password;
+    url_with_password = config.url_db.replace("${p}", password);
+    mongodb = await MongoClient.connect(url_with_password, {
+            poolSize: 3,
+            useUnifiedTopology: true 
+        });
+    mongodb = mongodb.db("trial_db");
+    Logger.info("DB connected");
+}
+function getDB(){
+    assert(mongodb != null);    
+    return mongodb;
+}
+
+
+connect();
+
+
+module.exports = {
+    getDB
+};
